f9a59302857e047f3062192cbfc530ea4e38a3eb
Jorginho <georgedanielmarques@gmail.com>
2024-10-23 12:19:01 -0300

    feat-Controller e Views de Evento

---
+++ eEvento/Controllers/EventosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eEvento.Models;
+
+namespace eEvento.Controllers
+{
+    public class EventosController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Eventos
+        public ActionResult Index()
+        {
+            var eventos = db.Eventos.Include(e => e.Local);
+            return View(eventos.ToList());
+        }
+
+        // GET: Eventos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Eventos.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // GET: Eventos/Create
+        public ActionResult Create()
+        {
+            ViewBag.LocalId = new SelectList(db.Locais, "LocalId", "Nome");
+            return View();
+        }
+
+        // POST: Eventos/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EventoId,Nome,Descricao,Data,LocalId,Capacidade")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Eventos.Add(evento);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.LocalId = new SelectList(db.Locais, "LocalId", "Nome", evento.LocalId);
+            return View(evento);
+        }
+
+        // GET: Eventos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Eventos.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.LocalId = new SelectList(db.Locais, "LocalId", "Nome", evento.LocalId);
+            return View(evento);
+        }
+
+        // POST: Eventos/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EventoId,Nome,Descricao,Data,LocalId,Capacidade")] Evento evento)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(evento).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.LocalId = new SelectList(db.Locais, "LocalId", "Nome", evento.LocalId);
+            return View(evento);
+        }
+
+        // GET: Eventos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Evento evento = db.Eventos.Find(id);
+            if (evento == null)
+            {
+                return HttpNotFound();
+            }
+            return View(evento);
+        }
+
+        // POST: Eventos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Evento evento = db.Eventos.Find(id);
+            db.Eventos.Remove(evento);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
