77130ed30ec3d5b381412482aa7e746e2ed96946
Jorginho <georgedanielmarques@gmail.com>
2024-10-24 09:46:06 -0300

    feat-Controller e Views de Participantes

---
+++ eEvento/Controllers/ParticipantesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eEvento.Models;
+
+namespace eEvento.Controllers
+{
+    public class ParticipantesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Participantes
+        public ActionResult Index()
+        {
+            return View(db.Participantes.ToList());
+        }
+
+        // GET: Participantes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participante participante = db.Participantes.Find(id);
+            if (participante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participante);
+        }
+
+        // GET: Participantes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Participantes/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ParticipanteId,Nome,Email,CPF")] Participante participante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Participantes.Add(participante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(participante);
+        }
+
+        // GET: Participantes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participante participante = db.Participantes.Find(id);
+            if (participante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participante);
+        }
+
+        // POST: Participantes/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ParticipanteId,Nome,Email,CPF")] Participante participante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(participante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(participante);
+        }
+
+        // GET: Participantes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Participante participante = db.Participantes.Find(id);
+            if (participante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(participante);
+        }
+
+        // POST: Participantes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Participante participante = db.Participantes.Find(id);
+            db.Participantes.Remove(participante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
