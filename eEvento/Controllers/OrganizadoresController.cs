e37eb3639310010a9bdddf0a1788078da08a2dbd
Jorginho <georgedanielmarques@gmail.com>
2024-10-24 10:03:50 -0300

    feat-Controllers e Views de Organizadores e Patrocinadores

---
+++ eEvento/Controllers/OrganizadoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eEvento.Models;
+
+namespace eEvento.Controllers
+{
+    public class OrganizadoresController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Organizadores
+        public ActionResult Index()
+        {
+            return View(db.Organizadores.ToList());
+        }
+
+        // GET: Organizadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organizador organizador = db.Organizadores.Find(id);
+            if (organizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizador);
+        }
+
+        // GET: Organizadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Organizadores/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "OrganizadorId,Nome,Contato")] Organizador organizador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Organizadores.Add(organizador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(organizador);
+        }
+
+        // GET: Organizadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organizador organizador = db.Organizadores.Find(id);
+            if (organizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizador);
+        }
+
+        // POST: Organizadores/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "OrganizadorId,Nome,Contato")] Organizador organizador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(organizador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(organizador);
+        }
+
+        // GET: Organizadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Organizador organizador = db.Organizadores.Find(id);
+            if (organizador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(organizador);
+        }
+
+        // POST: Organizadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Organizador organizador = db.Organizadores.Find(id);
+            db.Organizadores.Remove(organizador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ eEvento/Controllers/PatrocinadoresController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using eEvento.Models;
+
+namespace eEvento.Controllers
+{
+    public class PatrocinadoresController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Patrocinadores
+        public ActionResult Index()
+        {
+            return View(db.Patrocinadores.ToList());
+        }
+
+        // GET: Patrocinadores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patrocinador patrocinador = db.Patrocinadores.Find(id);
+            if (patrocinador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patrocinador);
+        }
+
+        // GET: Patrocinadores/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Patrocinadores/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PatrocinadorId,Nome,Contato")] Patrocinador patrocinador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Patrocinadores.Add(patrocinador);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(patrocinador);
+        }
+
+        // GET: Patrocinadores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patrocinador patrocinador = db.Patrocinadores.Find(id);
+            if (patrocinador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patrocinador);
+        }
+
+        // POST: Patrocinadores/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PatrocinadorId,Nome,Contato")] Patrocinador patrocinador)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(patrocinador).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(patrocinador);
+        }
+
+        // GET: Patrocinadores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Patrocinador patrocinador = db.Patrocinadores.Find(id);
+            if (patrocinador == null)
+            {
+                return HttpNotFound();
+            }
+            return View(patrocinador);
+        }
+
+        // POST: Patrocinadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Patrocinador patrocinador = db.Patrocinadores.Find(id);
+            db.Patrocinadores.Remove(patrocinador);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
